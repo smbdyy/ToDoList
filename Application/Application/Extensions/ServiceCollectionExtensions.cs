f13241cc8ffb5563908dc4f42fc1975208a1a44b
Ilya <ividivi67@gmail.com>
2023-04-22 13:32:35 +0300

    feat: add AddApplication service collection ext.

---
+++ Application/Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection collection)
+    {
+        collection.AddMediatR(typeof(IAssemblyMarker));
+        return collection;
+    }
+}
