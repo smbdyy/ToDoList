9cba73b91e05873a9aa9a00022b46de69b3bf80e
Ilya <ividivi67@gmail.com>
2023-04-22 13:29:33 +0300

    feat: add DTO mapping extensions

---
+++ Application/Application/Mapping/BoardMapping.cs
@@ -0,0 +1,10 @@
+﻿using Application.Dto;
+using Domain.Tasks;
+
+namespace Application.Mapping;
+
+public static class BoardMapping
+{
+    public static BoardDto AsDto(this Board board)
+        => new BoardDto(board.Id, board.Name.Value, board.Tasks.Select(task => task.AsDto()).ToArray());
+}
---
+++ Application/Application/Mapping/ToDoTaskMapping.cs
@@ -0,0 +1,10 @@
+﻿using Application.Dto;
+using Domain.Tasks;
+
+namespace Application.Mapping;
+
+public static class ToDoTaskMapping
+{
+    public static ToDoTaskDto AsDto(this ToDoTask task)
+        => new ToDoTaskDto(task.Id, task.Text.Value, (int)task.Priority, task.IsCompleted, task.Board.Id);
+}
