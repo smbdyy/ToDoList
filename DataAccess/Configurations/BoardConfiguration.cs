912460096c6388646d5563bf3448287357d20a5d
Ilya <ividivi67@gmail.com>
2023-04-22 12:30:30 +0300

    feat: add entities configuration

---
+++ DataAccess/Configurations/BoardConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations;
+
+public class BoardConfiguration : IEntityTypeConfiguration<Board>
+{
+    public void Configure(EntityTypeBuilder<Board> builder)
+    {
+        builder.Navigation(x => x.Tasks).HasField("_tasks").UsePropertyAccessMode(PropertyAccessMode.Field);
+    }
+}
---
+++ DataAccess/Configurations/ToDoTaskConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations;
+
+public class ToDoTaskConfiguration : IEntityTypeConfiguration<ToDoTask>
+{
+    public void Configure(EntityTypeBuilder<ToDoTask> builder)
+    {
+        builder.HasOne(x => x.Board).WithMany(x => x.Tasks);
+    }
+}
