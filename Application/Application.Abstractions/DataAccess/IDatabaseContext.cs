3ff12047df50c6f147271fb6af30374a1be71670
Ilya <ividivi67@gmail.com>
2023-04-22 12:35:05 +0300

    feat: add database context abstraction

---
+++ Application/Application.Abstractions/DataAccess/IDatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Abstractions.DataAccess;
+
+public interface IDatabaseContext
+{
+    DbSet<Board> Boards { get; }
+    DbSet<ToDoTask> Tasks { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
